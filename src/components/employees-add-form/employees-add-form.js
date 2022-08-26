8b1f1ea289d2e0e929728af3bafef22b9de48b1c
Viktoriia <rudneva15v@gmail.com>
2022-08-26 16:59:44 +0200

    commit 125

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,36 @@
+
+import {Component} from react;
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Add new employees</h3>
-            <form className="add form d-flex">
-                <input type="text"
-                className="form control new-post-label" 
-                placeholder="name"/>
-                
-                <input type="number"
-                className="form control new-post-label" 
-                placeholder="$?"/>
-             <button type="submit"
-             className="btn btn-outline-light">Add</button>
-            </form>
+class EmployeesAddForm extends Component {
+   constructor(props) {
+    super(props);
+  this.state = {
+    
+  }
+   }
 
-        </div>
-    )
+
+
+    render() {
+        return (
+            <div className="app-add-form">
+                <h3>Add new employees</h3>
+                <form className="add form d-flex">
+                    <input type="text"
+                    className="form control new-post-label" 
+                    placeholder="name"/>
+                    
+                    <input type="number"
+                    className="form control new-post-label" 
+                    placeholder="$?"/>
+                 <button type="submit"
+                 className="btn btn-outline-light">Add</button>
+                </form>
+    
+            </div>
+        )
+    }
 }
 
 
